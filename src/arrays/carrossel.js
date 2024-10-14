3884c2f4b0b545ec1ba949507ae60a1d9367a50c
douglasnou <dodonou3@gmail.com>
2024-10-14 12:54:49 -0300

    adição do carrossel com Swiper

---
+++ src/arrays/carrossel.js
@@ -0,0 +1,16 @@
+import Image from "../assets/slide.png";
+import Image1 from "../assets/slide1.png";
+import Image2 from "../assets/slide2.png";
+import Image3 from "../assets/slide3.png";
+import Image4 from "../assets/slide4.png";
+import Image5 from "../assets/slide5.png";
+import Image6 from "../assets/slide6.png";
+import Image7 from "../assets/slide7.png";
+import Image8 from "../assets/slide8.png";
+import Image9 from "../assets/slide9.png";
+import Image10 from "../assets/slide10.png";
+import Image11 from "../assets/slide11.png";
+import Image12 from "../assets/slide12.png";
+import Image13 from "../assets/slide13.png";
+
+export const Carroussel = [Image, Image1, Image2, Image3, Image4, Image5, Image6, Image7, Image8, Image9, Image10, Image11, Image12, Image13,]
--- src/components/AboutSection/index.jsx
+++ src/components/AboutSection/index.jsx
@@ -1,28 +1,48 @@
-import Praca from "../../assets/praca-jucaral.png";
+import React from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import styled from "./style.module.scss";
+import { Carroussel } from "../../arrays/carrossel";
+
 
 export const AboutSection = () => {
     return (
         <section className={styled.AboutSection}>
             <div className={styled.TextSection}>
-                <h2 className={`about-title  `}>O BAIRRO PLANEJADO
-                    + COMPLETO E DESEJADO
-                    DA REGIÃO</h2>
-                <p className={`about-text`}>
+                <h2 className="about-title">O BAIRRO PLANEJADO + COMPLETO E DESEJADO DA REGIÃO</h2>
+                <p className="about-text">
                     No <strong>Ecoville Pedreiras</strong>, cada dia é uma nova oportunidade para criar lembranças inesquecíveis com a sua família.
                 </p>
-                <p className={`about-text`}>
+                <p className="about-text">
                     Temos <strong>+ de 20 itens</strong> de lazer com espaços pensados para seu viver e bem-estar. Com áreas verdes, espaço para seu pet, playgrounds para as crianças, áreas de vivência e qualidade de vida ao ar livre.
                 </p>
 
-                <a className={`about-btn`} href="https://wa.me/559888385253/?text=Ol%C3%A1,%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20o%20Ecoville%20Pedreiras!%F0%9F%92%9A%F0%9F%A4%A9" target="blank">
+                <a className="about-btn" href="https://wa.me/559888385253/?text=Ol%C3%A1,%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20o%20Ecoville%20Pedreiras!%F0%9F%92%9A%F0%9F%A4%A9" target="_blank">
                     QUERO COMPRAR
                 </a>
             </div>
 
             <div className={styled.AboutImg}>
-                <img src={Praca} />
+                <Swiper
+                    modules={[Navigation, Pagination, Autoplay]}
+                    spaceBetween={30}
+                    slidesPerView={1}
+                    navigation
+                    pagination={{ clickable: true }}
+                    autoplay={{ delay: 3000, disableOnInteraction: false }}
+                    loop={true}>
+                    {
+                        Carroussel.map((item) => (
+                            <SwiperSlide>
+                                <img src={item} alt="Ecoville Pedreiras" />
+                            </SwiperSlide>
+                        ))
+                    }
+                </Swiper>
             </div>
         </section>
-    )
-}
+    );
+};
