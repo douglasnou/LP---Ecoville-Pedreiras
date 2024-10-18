627e08770b0f5e00c6a77a0ca2e8db6ec73698ab
douglasnou <dodonou3@gmail.com>
2024-10-18 11:54:01 -0300

    mudança dinâmica de img conforme width

--- src/components/BannerSection/index.jsx
+++ src/components/BannerSection/index.jsx
@@ -1,10 +1,12 @@
 import Banner from "../../assets/banner.png";
-import styled from "./style.module.scss"
+import BannerSmall from "../../assets/banner-small.png";
+import styled from "./style.module.scss";
 
-export const BannerSection = () =>{
-    return(
-        <section className={styled.BannerSection}>
-            <img src={Banner} />
-        </section>
-    )
+export const BannerSection = () => {
+  return (
+    <section className={styled.BannerSection}>
+      <img src={Banner} className={styled.BannerLarge} alt="Banner" />
+      <img src={BannerSmall} className={styled.BannerSmall} alt="Banner pequeno" />
+    </section>
+  )
 }
