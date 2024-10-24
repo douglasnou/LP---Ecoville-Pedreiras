8bf93e147a8c3fb1ac32828ab8b99673f4408b37
douglasnou <dodonou3@gmail.com>
2024-10-24 13:41:18 -0300

    adição do google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { AboutSection } from "./components/AboutSection";
 import { BannerSection } from "./components/BannerSection";
 import { Citysection } from "./components/CitySection";
@@ -7,9 +8,16 @@ import { ParkSection } from "./components/ParkSection";
 import { SecuritySection } from "./components/SecuritySection";
 import { UnidadeSection } from "./components/UnidadeSection";
 import { VideoSection } from "./components/VideoSection";
-import "./styles/index.scss"
+import ReactGA from "react-ga4"
+import "./styles/index.scss";
 
 function App() {
+
+  useEffect(()=>{
+    ReactGA.initialize("G-1XLHJN1Y8L");
+
+    ReactGA.send("pageview");
+  }, [])
 
   return (
     <>
